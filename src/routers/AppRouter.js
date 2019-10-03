eeb8c0c2919a0e5b517ab9df4fd998d4a4e76d52
Pavan Tank <pavantank7016@gmail.com>
2019-10-03 11:52:23 +0530

    changes 4

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import '../styles/styles.scss';
-import DashboardExpensePage from '../components/dashboardExpensePage';
-import DashboardPage from '../components/dashboardPage';
+import DashboardExpensePage from '../components/DashboardExpensePage';
+import DashboardPage from '../components/DashboardPage';
 import EditExpensePage from '../components/EditExpensePage';
 import Header from '../components/Header.js';
 import HelpPage from '../components/HelpPage.js';
